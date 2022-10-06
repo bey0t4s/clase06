5c44fb88b718e599788b6eb272ed4c4ddaf66e25
Amalia <rosalopez@roagro.com.ar>
2022-10-06 18:06:09 -0300

    js

--- js/clasejs.js
+++ js/clasejs.js
@@ -8,8 +8,27 @@ let b;
 const nombreConstante = 222;
 
 nombre = "prueba 1";
-
+console.log("=================================================");
 console.log("primer variable: " + nombre + nombreConstante);
 
 console.log(`primer variable: ${nombre}  nombreConstante`); 
+console.log("=================================================");
+//Arrays
+
+let banana = "banana";
+let manzana = "manzana";
+
+let frutas = ['manzanas', 'peras', banana];
+console.log(`las frutas son: ${frutas}`);
+
+let persona = ['pirulo', 12345, true];
+console.log(`los datos de personas son: ${persona}`);
+
+console.log("=================================================");
+
+//objetos
+
+
+
+
 
