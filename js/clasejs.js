72fd7c937fd746d917722701f055e35e566f9888
Amalia <rosalopez@roagro.com.ar>
2022-10-06 17:28:19 -0300

    clase06

---
+++ js/clasejs.js
@@ -0,0 +1,15 @@
+console.log("hola mundo");
+
+//forma de definir variables
+var nombre;
+
+let b;
+
+const nombreConstante = 222;
+
+nombre = "prueba 1";
+
+console.log("primer variable: " + nombre + nombreConstante);
+
+console.log(`primer variable: ${nombre}  nombreConstante`); 
+
